11bbff0c22be7489d28e561952ab7206288fc39e
cometdlut <cometdlut@qq.com>
2017-10-03 15:29:52 +0800

    Add two number sum solution

---
+++ TwoSum.py
@@ -0,0 +1,33 @@
+# _*_ coding:utf-8 _*_
+
+"""
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+"""
+
+class Solution():
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i in range(len(nums)):
+            if nums[i] in dict:
+                return [dict[nums[i]], i]
+            else:
+                dict[target - nums[i]] = i
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print sol.twoSum([2,7,11,15], 9)
+    
